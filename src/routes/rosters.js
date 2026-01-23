85c40c96e9894db6f8ca2d95b75aedfe706c6f6a
Ives905 <64328762+Ives905@users.noreply.github.com>
2026-01-23 09:00:23 -0500

    Update rosters.js

--- src/routes/rosters.js
+++ src/routes/rosters.js
@@ -1,6 +1,6 @@
 // routes/rosters.js - Updated to use rounds table for lock dates
 const express = require('express');
-const pool = require('../config/database');
+const pool = require('../../config/database');
 const { authenticateToken } = require('../middleware/auth');
 
 const router = express.Router();
